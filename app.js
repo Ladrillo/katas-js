8867ec96eae3cdb746eb17cce8e0fbb1f44b95a9
Gabriel Cabrejas Poole <gabriel@cabrejas.net>
2016-01-13 14:56:26 -0700

    refactoring

--- app.js
+++ app.js
@@ -1,32 +1,3 @@
 import { codewars } from './challenges/codewars';
 
 codewars();
-
-class Square {
-    constructor(side) {
-        this._side = side * 1000;
-    }
-    get side() {
-        return `the side of this square is ${this._side} mm`;
-    }
-    set side(newSide) {
-        this._side = newSide * 1000;
-    }
-}
-
-let mySquare = new Square(4);
-
-console.log(mySquare.side);
-
-mySquare.side = 3;
-
-Object.defineProperty(mySquare, "cosa", {value:'tonta', writable:false});
-
-
-console.log(mySquare);
-// console.log(mySquare.cosa);
-
-Object.freeze(mySquare);
-mySquare.side = 5;
-console.log(mySquare);
-// mySquare.cosa = 'not happening';
--- challenges/codewars.js
+++ challenges/codewars.js
@@ -238,10 +238,10 @@ export function codewars() {
     //A digital root is the recursive sum of all the digits in a number. Given n, take the sum of the digits of n. If that value has two digits, continue reducing in this way until a single-digit number is produced. This is only applicable to the natural numbers.
     
     let digitalRoot = function (int) {
+        
         if (int < 10) return int;
         let digitSum = String(int).split('').map(e => Number(e)).reduce((acc, e) => acc + e);
-        if (digitSum < 10) return digitSum;
-        return digitalRoot(digitSum);
+        return digitSum < 10 ? digitSum : digitalRoot(digitSum);
     };
 
     describe('function digitalRoot', function () {
